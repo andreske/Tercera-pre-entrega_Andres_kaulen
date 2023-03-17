f587e28e8d0c68e4eeed053ce1d992cd80f6a1ec
Andres <andres.kaulen@gmail.com>
2023-03-17 18:58:33 -0300

    modelo creado

--- AppWeb/models.py
+++ AppWeb/models.py
@@ -5,3 +5,17 @@ class Home(models.Model):
 
     nombre = models.CharField(max_length=40)
 
+
+class Usuario(models.Model):
+
+    nombre= models.CharField(max_lenght=30)
+    apellido= models.CharField(max_lenght=30)
+    email= models.EmailField()
+
+class Solicitud(models.Model):
+
+    cantidad= models.IntegerField()
+
+class Direcciones(models.Model):
+
+    nombre_pila=
