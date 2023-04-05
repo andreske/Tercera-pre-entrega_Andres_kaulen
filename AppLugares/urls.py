9b711966f3a3722b4b91d4955fb9dacbb81293f8
Andres <andres.kaulen@gmail.com>
2023-04-05 16:41:43 -0400

    Post guardado 2

--- AppLugares/urls.py
+++ AppLugares/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from AppLugares.views import render_posts, post_detail
 
+app_name = "AppLugares"
+
 urlpatterns = [
     path('', render_posts, name='post'),
-    path('<int:post_id>', post_detail)
+    path('<int:post_id>', post_detail, name="post_detail")
 ]
 
 
